b1115936d94633fca6cbab6079aeb8f0045623a0
Erik Hedin <erik123567@gmail.com>
2018-03-03 13:38:47 -0500

    network attempt

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class MyNetworkManager : NetworkManager {
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void MyStartHost ()
+	{
+		Debug.Log("Starting host at " + Time.timeSinceLevelLoad);
+		StartHost();
+	}
+
+	public override void OnStartHost(){
+		Debug.Log("host started at " + Time.timeSinceLevelLoad);
+	}
+
+	public override void OnStartClient (NetworkClient myClient)
+	{
+		Debug.Log(Time.timeSinceLevelLoad+ "clinet start requested");
+		InvokeRepeating("PrintDots", 0f, 1f);
+	}
+
+	public override void OnClientConnect (NetworkConnection conn)
+	{
+		Debug.Log(Time.timeSinceLevelLoad + " client is connected to IP " + conn.address);
+		CancelInvoke();
+	}
+
+	void PrintDots(){
+		Debug.Log(".");
+	}
+}
